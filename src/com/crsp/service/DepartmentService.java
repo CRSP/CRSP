1def36afdde392e9f626af8bb51bf4218ed557ae
11080716 <947218223@qq.com>
2014-03-29 20:14:38 +0800

    UserService

--- src/com/crsp/service/DepartmentService.java
+++ src/com/crsp/service/DepartmentService.java
@@ -1,11 +1,7 @@
 package com.crsp.service;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.crsp.dao.DepartmentDAO;
 import com.crsp.entity.Department;
 
@@ -14,62 +10,23 @@ import com.crsp.entity.Department;
 public class DepartmentService {
 	@Autowired
 	private DepartmentDAO departmentDAO;
-	@Autowired
-	private SessionFactory sessionFactory;
-	
 	//添加院系
 	public Department addDepartment(Department department){
 		
-		Session session=sessionFactory.getCurrentSession();
-		Transaction tr=null;
-		try{
-			tr=session.beginTransaction();
-			 departmentDAO.save(department);
-			tr.commit();			
-		}
-		catch (Exception e) {
-			department=null;
-			System.out.println("插入错误");
-			tr.rollback();
-		}
-		
-		return department;						
+		 departmentDAO.save(department);
+		 return  departmentDAO.findById(department.getId());	
 	}
-	
 	//更新信息
-	public boolean updateInformation(Department department){
-		boolean flag=false;
-		Session session = sessionFactory.getCurrentSession();
-		Transaction tr = null;	
-		try {
-			tr = session.beginTransaction();			
-			departmentDAO.update(department);
-			tr.commit();
-			flag=true;
-		} catch (Exception e) {
-			System.out.println("保存错误！");
-			e.printStackTrace();
-			tr.rollback();
-		}		
-		return flag;
+	public void updateInformation(Department department){
 	
+	departmentDAO.update(department);
+		
 	}
 	
 	//查看信息
 	public Department seeInformation(int department_id){
-		Session session = sessionFactory.getCurrentSession();
-		Transaction tr = null;	
-		Department department=null;
-		try {
-			tr = session.beginTransaction();
-			department=departmentDAO.findById(department_id);
-			tr.commit();			
-		} catch (Exception e) {
-			System.out.println("查看错误！");
-			e.printStackTrace();
-			tr.rollback();
-		}							
-		return department;
+	
+		return  departmentDAO.findById(department_id);						
 		
 	}
 	
--- src/com/crsp/service/SchoolService.java
+++ src/com/crsp/service/SchoolService.java
@@ -3,98 +3,49 @@ package com.crsp.service;
 
 
 import java.util.List;
-
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.crsp.dao.SchoolDAO;
 import com.crsp.entity.School;
-import com.crsp.entity.User;
 import com.crsp.entity.Department;
 
 @Service
 public class SchoolService {
 	@Autowired
 	private SchoolDAO schoolDAO;
-	@Autowired
-	private SessionFactory sessionFactory;
+
 	
 	
 	//添加学校
 	public School addSchool(School school){
 		schoolDAO.save(school);
-		return school;
+		return schoolDAO.findById(school.getId());
 	}
 	//更新学校信息
-	public boolean updateInformation(School school){
-		boolean flag=false;
-		Session session = sessionFactory.getCurrentSession();
-		Transaction tr = null;	
-		try {
-			tr = session.beginTransaction();			
-			schoolDAO.update(school);
-			tr.commit();
-			flag=true;
-		} catch (Exception e) {
-			System.out.println("保存错误！");
-			e.printStackTrace();
-			tr.rollback();
-		}		
-		return flag;					
+	public void updateInformation(School school){
+		
+	schoolDAO.update(school);
+		
 	}
 	//查询学校信息
 	public School seeInformation(int school_id){
-		Session session = sessionFactory.getCurrentSession();
-		Transaction tr = null;	
-		School school=null;
-		try {
-			tr = session.beginTransaction();
-			school=schoolDAO.findById(school_id);
-			tr.commit();			
-		} catch (Exception e) {
-			System.out.println("查看错误！");
-			e.printStackTrace();
-			tr.rollback();
-		}							
-		return school;
 
+		return schoolDAO.findById(school_id);
 	}
 	
     //查找所开设的院系
 	public List<Department> findDepartment(int school_id){
-		Session session = sessionFactory.getCurrentSession();
-		Transaction tr = null;	
-		List<Department> list=null;
-		try {
-			tr = session.beginTransaction();
-			list=(List<Department>)schoolDAO.findDepartments(school_id);
-			tr.commit();			
-		} catch (Exception e) {
-			System.out.println("查看错误！");
-			e.printStackTrace();
-			tr.rollback();
-		}		
-		return list;
+
+		return 	(List<Department>)schoolDAO.findDepartments(school_id);
+		
 	}	
 	
 	//查找各地区的学校
 	public List<School> findSchool(int province_id){
-		Session session = sessionFactory.getCurrentSession();
-		Transaction tr = null;	
-		List<School> list=null;
-		try {
-			tr = session.beginTransaction();
-			list=(List<School>)schoolDAO.findByProvince(province_id);
-			tr.commit();			
-		} catch (Exception e) {
-			System.out.println("查看错误！");
-			e.printStackTrace();
-			tr.rollback();
-		}		
-		return list;	
+	
+		return (List<School>)schoolDAO.findByProvince(province_id);
+	
+		
 	}
 	
 	
