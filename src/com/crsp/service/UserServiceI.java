59c2e2a5b8c6167a40aff5643cc833c05b8c1bcd
huangruichang <532079207@qq.com>
2014-04-22 16:51:38 +0800

    fix UserServiceI

--- src/com/crsp/service/UserServiceI.java
+++ src/com/crsp/service/UserServiceI.java
@@ -1,12 +1,11 @@
 package com.crsp.service;
 
-import java.util.List;
-
 import com.crsp.dto.RecordDTO;
 import com.crsp.dto.ResourceDTO;
 import com.crsp.dto.UserDTO;
 import com.crsp.entity.User;
 import com.crsp.utils.Page;
+import com.crsp.utils.Pages;
 
 public interface UserServiceI {
 	//装入id, user_name, school_name, department_name
@@ -16,11 +15,11 @@ public interface UserServiceI {
 	public UserDTO register(User user);	//用户注册
 	
 	//每个ResourceDTO装入id, resource_name, status_name, time, type
-	public List<ResourceDTO> getMyUpload(int userId, Page page);
+	public Pages<ResourceDTO> getMyUpload(int userId, Page page);
 	
 	//每个ResourceDTO装入id, resource_name, status_name, time, type
-	public List<ResourceDTO> getMyDownload(int userId, Page page);
+	public Pages<ResourceDTO> getMyDownload(int userId, Page page);
 	
 	//每个RecordDTO装入id, delta, time, 
-	public List<RecordDTO> getMyRecord(int userId, Page page);
+	public Pages<RecordDTO> getMyRecord(int userId, Page page);
 }
